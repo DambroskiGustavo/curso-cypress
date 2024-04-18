4d1167800d81c8075b73d0885b69ba2a3eaacd05
Gustavo Dambroski <gustavodambroski@gmail.com>
2024-04-17 23:45:20 -0300

    more content about basics interactions

--- cypress/e2e/3-course-activities/curso-basic.cy.js
+++ cypress/e2e/3-course-activities/curso-basic.cy.js
@@ -1,16 +1,20 @@
 /// <reference types="cypress" />
 
 describe('Cypress basics', () => { 
-    it('Should visit a page and assert title', () => {
+    it.only('Should visit a page and assert title', () => {
         cy.visit('https://www.wcaquino.me/cypress/componentes.html') // visit = visita/acessa
         
         // const title = cy.title()
         // console.log(title)
 
+        /* Validação de Pause */
+        // cy.pause() = Usa o metótodo de pausar a aplicação
+
         /* Uso do SHOULD para validar */
         cy.title().should('be.equal', 'Campo de Treinamento') // should (deve) + be equal = ser igual
         cy.title().should('contain', 'Campo') //  should (deve) + contain = deve conter
-
+            // .debug() = usa o debug do console para depurar o código
+        
         /* Uso de forma mais "estruturada" e "legível" */
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
@@ -20,7 +24,7 @@ describe('Cypress basics', () => {
         //TODO escrever o log em um campo de texto
     })
 
-    it.only('Should find and interact with an element', () => {
+    it('Should find and interact with an element', () => {
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')      
 
         cy.get('#buttonSimple')
