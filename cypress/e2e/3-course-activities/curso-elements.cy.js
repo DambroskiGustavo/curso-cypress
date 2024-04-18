1ef08aad7f9818a6538094a624ac5023d9cc651a
Gustavo Dambroski <gustavodambroski@gmail.com>
2024-04-18 09:04:10 -0300

    new class about find elements by text

---
+++ cypress/e2e/3-course-activities/curso-elements.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+/// <reference types="cypress" />
+
+/* Validações de elementos por texto */
+describe('Worj with basic elements', () => { 
+    it.only('Text...', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    //  cy.get('body').should('contain', 'Cuidado') // JQuery selector + busca via body (muito ampla)
+    //  cy.get('body').should('have.text', 'Cuidado') // Have text busca o texto exato (não considera apenas o contain)
+        cy.get('span').should('contain', 'Cuidado') // JQuery selector + busca via span ("menos" amplo)
+        /* Validação mais adequada */
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...') // have.text = texto exato
+    })
+})        
