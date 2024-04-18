9dae53120bc5a27a4b230b315bc5b05298a0128a
Gustavo Dambroski <gustavodambroski@gmail.com>
2024-04-18 11:44:22 -0300

    new class about hooks

--- cypress/e2e/3-course-activities/curso-elements.cy.js
+++ cypress/e2e/3-course-activities/curso-elements.cy.js
@@ -1,25 +1,29 @@
 /// <reference types="cypress" />
 
-describe('Worj with basic elements', () => { 
+describe('Work with basic elements', () => { 
+    // before(() => { // before = comando de realizar ação(ou ações) específicamente antes dos testes
+    beforeEach(() => { // before = comando de realizar ação(ou ações) específicamente antes de cada teste
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+
     /* Validações de elementos por texto */
     it('Text...', () => {
-        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
-    //  cy.get('body').should('contain', 'Cuidado') // JQuery selector + busca via body (muito ampla)
+        cy.get('body').should('contain', 'Cuidado') // JQuery selector + busca via body (muito ampla)
     //  cy.get('body').should('have.text', 'Cuidado') // Have text busca o texto exato (não considera apenas o contain)
         cy.get('span').should('contain', 'Cuidado') // JQuery selector + busca via span ("menos" amplo)
         /* Validação mais adequada */
+        cy.get('.facilAchar').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...') // have.text = texto exato
         // "." é o identificador de classes
     })
 
-    it.only('Links', () => {
+    it('Links...', () => {
         /* Validações de elementos por links */
-        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
         // usando o href, ou "a"
         cy.get('[href="#"]').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
         // "#" é o identificador de id
-
+        
         cy.reload()
         cy.get('#resultado').should('have.not.text', 'Voltou!')
         cy.contains('Voltar').click() // usando o contais para validar o elemento (não é a melhor práticas)
