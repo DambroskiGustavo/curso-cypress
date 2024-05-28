36cfea6f8bb5ddc5922fbb1be8ff382b94eda238
Gustavo Dambroski <gustavodambroski@gmail.com>
2024-05-28 16:56:42 -0300

    new class about Its and Invoke

--- cypress/e2e/3-course-activities/curso-herlpers.cy.js
+++ cypress/e2e/3-course-activities/curso-herlpers.cy.js
@@ -40,4 +40,38 @@ describe('Helpers...', () => {
                     return 2
         }).should('be.equal', 2)
     })
+
+    it('Its...', () => {
+        const obj = {nome: 'User', idade: 20}
+        cy.wrap(obj).should('have.property', 'nome', 'User')
+        cy.wrap(obj).its('nome').should('be.equal', "User")
+
+        const obj2 = {nome: 'User', idade: 20, endereco: { rua: 'dos bobos' }}
+        cy.wrap(obj2).its('endereco.rua').should('contain', 'bobos') // its(A.B) = encadeia a chamada usando o its
+
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        cy.title().its('length').should('be.equal', 20)
+    })
+
+    it.only('Invoke...', () => {
+        const getValue = () => 1; 
+        const soma = (a, b) => a + b;
+
+        cy.wrap({fn: getValue})
+            .invoke('fn') // invoke = invoca a função desejada
+            .should('be.equal', 1)
+
+        cy.wrap({fn: soma})
+            .invoke('fn', 2, 5) 
+            .should('be.equal', 7)  
+
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        cy.get('#formNome').invoke('val', 'Texto via invoke') // o "type" é mais recomendado (normal)
+
+        cy.window().invoke('alert', 'da pra ver?')
+        cy.get('#resultado')
+           .invoke('html', '<input type="button" value="hacked!"/>')
+    
+    })
+
 })
