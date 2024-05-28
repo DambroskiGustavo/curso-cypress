2e6478bc3478c24d89e5f5b8e7616f05e0c12df7
Gustavo Dambroski <gustavodambroski@gmail.com>
2024-05-28 17:34:11 -0300

    another class to resolve some "to dos"

--- cypress/e2e/3-course-activities/curso-elements.cy.js
+++ cypress/e2e/3-course-activities/curso-elements.cy.js
@@ -90,14 +90,43 @@ describe('Work with basic elements', () => {
     
     it('Combo', () => {
         cy.get('[data-test="dataEscolaridade"]')
+            .select('2o grau completo') // no select, é possível selecionar o valor visível ou o valor do campo (código)
+            .should('have.value', '2graucomp') // no should, só é possível validar o valor do campo (código)
+
+        cy.get('[data-test="dataEscolaridade"]')
             .select('1graucomp') // no select, é possível selecionar o valor visível ou o valor do campo (código)
-            .should('have.value', '1graucomp') // no should, só é possível validar o valor do campo (código)
+            .should('have.value', '1graucomp')
+
+        cy.get('[data-test="dataEscolaridade"] option')
+            .should('have.length', 8)
+
+        cy.get('[data-test="dataEscolaridade"] option')
+            .then($arr => {
+                const values = []
+                $arr.each(function() {
+                    values.push(this.innerHTML)
+                })
+                expect(values).to.include.members(["Superior", "Mestrado"])
+            })
+
     })
 
     it.only('Combo multiplo', () => {
         cy.get('[data-testid="dataEsportes"]')
             .select(['natacao', 'Corrida', 'nada']) // via array, o select deve ser feito pelo 'value', não pela string visível
 
-    })
+        //cy.get('[data-testid="dataEsportes"]')
+          //  .should('have.value', ['natacao', 'Corrida', 'nada'])
 
+        cy.get('[data-testid="dataEsportes"]')
+            .then($el => {
+                expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada'])
+                expect($el.val()).to.have.length(3)
+
+        cy.get('[data-testid="dataEsportes"]')
+            .invoke('val')
+            .should('eql', ['natacao', 'Corrida', 'nada'])
+
+        })
+    })
 })        
