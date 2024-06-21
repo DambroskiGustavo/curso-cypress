0ad21ba1b488a3d9a80f425746ca76d50ad4a51e
Gustavo Dambroski <gustavodambroski@gmail.com>
2024-06-21 11:18:02 -0300

    new class to organize locators

--- cypress/e2e/3-course-activities/barriga/functional.cy.js
+++ cypress/e2e/3-course-activities/barriga/functional.cy.js
@@ -1,46 +1,34 @@
 /// <reference types="cypress" />
 
+import loc from '../../../support/locators'
+
 describe('should test at a functional level', () => {
     beforeEach(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
-        cy.get('.input-group > .form-control')
-            .type('gustavodambroski@gmail.com')
-        cy.get(':nth-child(2) > .form-control')
-            .type('gustavo621')
-        cy.get('.btn')
-            .click()
-        cy.get('.toast-message')
-            .should('contain', 'Bem vindo')
+        cy.get(loc.LOGIN.USER).type('gustavodambroski@gmail.com')
+        cy.get(loc.LOGIN.PASSWORD).type('gustavo621')
+        cy.get(loc.LOGIN.BTN_LOGIN).click()
+        cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
     })
 
     it('Should create an account', () => {
         // Criando uma conta
-        cy.get('[data-test="menu-settings"]')
-            .click()
-        cy.get('[href="/contas"]')
-            .click()
-        cy.get('[data-test="nome"]')
-            .type('Conta de teste')
-        cy.get('.btn')
-            .click()
-        cy.get('.toast-message')
-            .should('contain', 'Conta inserida com sucesso!')
+        cy.get(loc.MENU.SETTINGS).click()
+        cy.get(loc.MENU.CONTAS).click()
+        cy.get(loc.CONTAS.NOME).type('Conta de teste')
+        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso!')
     })
-    
+
     it('Should update an account', () => {
         // Alterando uma conta
-        cy.get('[data-test="menu-settings"]')
-            .click()
-        cy.get('[href="/contas"]')
-            .click()
-        cy.xpath("//table//td[contains(., 'Conta de teste')]/..//i[@class='far fa-edit']")
-            .click()
-        cy.get('[data-test="nome"]')
+        cy.get(loc.MENU.SETTINGS).click()
+        cy.get(loc.MENU.CONTAS).click()
+        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
+        cy.get(loc.CONTAS.NOME)
             .clear()    
             .type('Conta alterada')
-        cy.get('.btn')
-            .click()
-        cy.get('.toast-message')
-            .should('contain', 'Conta atualizada com sucesso!')
+        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso!')
     })
 })
