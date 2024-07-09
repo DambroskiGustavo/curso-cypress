fcddf58bff221809c3d86a62f22d7a51b04ea213
Gustavo Dambroski <gustavodambroski@gmail.com>
2024-07-09 14:55:23 -0300

    new class to create an account by API

--- cypress/e2e/3-course-activities/barriga/backend.cy.js
+++ cypress/e2e/3-course-activities/barriga/backend.cy.js
@@ -20,6 +20,21 @@ describe('should test at a functional level', () => {
             senha: "gustavo621"
         }
     }).its('body.token').should('not.be.empty')
+    .then(token => {
+        cy.request({
+            method: 'POST',
+            headers: { Authorization: `JWT ${token}`}, // O uso de "JWT" é devido a versão da API, então em aplicações mais novas, se utilizará o "bearer" + ${token}
+            url: 'https://barrigarest.wcaquino.me/contas',
+            body: {
+                nome: 'Conta via rest'
+            }
+        }).as('response')
+    })
+    cy.get('@response').then(res =>{
+        expect(res.status).to.be.equal(201)
+        expect(res.body).to.have.property('id')
+        expect(res.body).to.have.property('nome','Conta via rest')
+    })
   })
 
   it('Should update an account', () => {
