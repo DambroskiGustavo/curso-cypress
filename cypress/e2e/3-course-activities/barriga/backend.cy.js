3c463d81338a5e2862dc31d91ea0ccd6dc01ab47
Gustavo Dambroski <gustavodambroski@gmail.com>
2024-07-10 14:15:42 -0300

    class to create a transaction and to install dayjs dependencies

--- cypress/e2e/3-course-activities/barriga/backend.cy.js
+++ cypress/e2e/3-course-activities/barriga/backend.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const dayjs = require('dayjs')
+
 describe('should test at a functional level', () => {
   let token
 
@@ -26,23 +28,17 @@ describe('should test at a functional level', () => {
         }).as('response')
 
     cy.get('@response').then(res =>{
-        expect(res.status).to.be.equal(201)
-        expect(res.body).to.have.property('id')
-        expect(res.body).to.have.property('nome','Conta via rest')
+      expect(res.status).to.be.equal(201)
+      expect(res.body).to.have.property('id')
+      expect(res.body).to.have.property('nome','Conta via rest')
     })
   })
 
   it('Should update an account', () => {
-    cy.request({
-      method: 'GET',
-      url: '/contas',
-      headers: { Authorization: `JWT ${token}`},
-      qs: {
-        nome: 'Conta para alterar'
-      }
-    }).then(res => {
+    cy.getContaByName('Conta para alterar')
+      .then(contaId => {
       cy.request({
-        url: `/contas/${res.body[0].id}`,
+        url: `/contas/${contaId}`,
         method: 'PUT',
         headers: { Authorization: `JWT ${token}`},
         body: {
@@ -54,7 +50,7 @@ describe('should test at a functional level', () => {
     cy.get('@response').its('status').should('to.equal', 200)
   })
 
-  it.only('should not create an account with same name', () => {
+  it('should not create an account with same name', () => {
     cy.request({
       method: 'POST',
       headers: { Authorization: `JWT ${token}`}, // O uso de "JWT" é devido a versão da API, então em aplicações mais novas, se utilizará o "bearer" + ${token}
@@ -66,14 +62,33 @@ describe('should test at a functional level', () => {
       }).as('response')
 
     cy.get('@response').then(res =>{
-      console.log(res)
-        expect(res.status).to.be.equal(400)
-        expect(res.body.error).to.be.equal('Já existe uma conta com esse nome!')
+      expect(res.status).to.be.equal(400)
+      expect(res.body.error).to.be.equal('Já existe uma conta com esse nome!')
     })
   })
 
   it('Should create a transaction', () => {
+    cy.getContaByName('Conta para movimentacoes')
+      .then(contaId => {
+        cy.request({
+          method: 'POST',
+          url: '/transacoes',
+          headers: { Authorization: `JWT ${token}`},
+          body: {
+            conta_id: contaId,
+            data_pagamento: dayjs().add(1, 'day').format('DD/MM/YYYY'),
+            data_transacao: dayjs().format('DD/MM/YYYY'),
+            descricao: "desc",
+            envolvido: "inter",
+            status: true,
+            tipo: "REC",
+            valor: "123"
+          }
+        }).as('response')
+    })
 
+    cy.get('@response').its('status').should('be.equal', 201)
+    cy.get('@response').its('body.id').should('exist')
   })
 
   it('Should get balance', () => {
